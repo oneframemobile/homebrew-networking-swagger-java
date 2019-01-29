db747f1b995a10aeb12e0161c037c968375c3d57
02483564 <umut.boz@kocsistem.com.tr>
2019-01-29 14:02:51 +0300

    1.3.0 ssh fix 16a25a18067851443282912e785099d589a9c4e3c6b8b1eeaa9115e4935ae8aa

--- networking-swagger-java.rb
+++ networking-swagger-java.rb
@@ -2,7 +2,7 @@ class NetworkingSwaggerJava < Formula
     desc "Networking-swagger java code generator"
     homepage "https://github.com/oneframemobile/homebrew-networking-swagger-java"
     url "https://github.com/oneframemobile/networking-swagger-java/archive/1.3.0.tar.gz"
-    sha256 "112b52d0f0a87e525c0e55a4b5cc6b5d0a0da1d3a957a184a1c20fd81ab3e121"
+    sha256 "16a25a18067851443282912e785099d589a9c4e3c6b8b1eeaa9115e4935ae8aa"
     version "1.3.0"
     # depends_on "cmake" => :build
 
